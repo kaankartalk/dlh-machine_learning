f356eae0cfbc2edbe2d3912dd28fc973bc4b8812
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-12 07:58:09 +0000

    4-line_up

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Adding two arrays element-wise.
+"""
+
+
+df add_arrays(arr1, arr2):
+    """
+    Adds two arrays element.
+    Returns a new list.
+    """
+    if len(arr1) != len(arr2):
+        return None
+
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+
+    return result
