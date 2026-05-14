2ab684ebbd856b315c8ec39ec8d8f5ff231fbede
root <kkaankartal@gmail.com>
2026-05-14 07:44:10 +0000

    5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,6 +3,7 @@
 Adds two 2D matrices element-wise.
 """
 
+
 def add_matrices2D(mat1, mat2):
     """Adds two 2D matrices element-wise."""
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
