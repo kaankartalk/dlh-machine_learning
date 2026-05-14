8dbb1d5e7d08fe427564cad5f9c859d1a70389c8
root <kkaankartal@gmail.com>
2026-05-14 08:39:50 +0000

    7-getting_cozy

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,6 +5,7 @@ Concatenates two 2D matrices along a given axis.
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
+    """Concatenates two 2D matrices along the specified axis."""
     if axis == 0:
         if len(mat1[0]) != len(mat2[0]):
             return None
