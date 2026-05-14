b62f726c57270b902861b58a9cbef7f3f6cf1eaf
root <kkaankartal@gmail.com>
2026-05-14 07:40:54 +0000

    5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,13 +3,11 @@
 Adds two 2D matrices element-wise.
 """
 
-
 def add_matrices2D(mat1, mat2):
-    # Check if shapes match
+    """Adds two 2D matrices element-wise."""
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
 
-    # Create new matrix with element-wise sum
     new_matrix = []
     for row1, row2 in zip(mat1, mat2):
         new_matrix.append([a + b for a, b in zip(row1, row2)])
