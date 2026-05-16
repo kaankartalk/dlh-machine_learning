fb2ebb06a92411d958e6809bb2f07cdf3586b4ae
Kaan Kartal KUYUCU <kkaankartal@gmail.com>
2026-05-16 11:14:13 +0000

    Task 14 completed

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Matrix multiplication using numpy.
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication."""
+    return np.matmul(mat1, mat2)
