2a208d533757724b29a7ced55d3ecc2740b1333a
Kaan Kartal KUYUCU <kkaankartal@gmail.com>
2026-05-16 11:05:08 +0000

    12-bracin

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-Module for element-wise operations on numpy ndarrays.
+Element-wise operations on numpy ndarrays.
 """
 
 
 def np_elementwise(mat1, mat2):
-    """Performs element-wise addition, subtraction, multiplication, and division"""
+    """Returns element-wise sum, difference, product, and quotient."""
     return (
         mat1 + mat2,
         mat1 - mat2,
