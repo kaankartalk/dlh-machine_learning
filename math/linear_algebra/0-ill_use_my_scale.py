6bdcf8f80d37adbd9d16ba30a242dbc0e1756c72
Kaan Kartal KUYUCU <kkaankartal@gmail.com>
2026-05-16 10:33:56 +0000

    0-ill

---
+++ math/linear_algebra/0-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Module for returning the shape of a numpy ndarray.
+"""
+
+def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray"""
+    return matrix.shape
