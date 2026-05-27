b5498284e65ec7a4cd9ca1b869bd966ea22c1e12
kaankartalk <kkaankartal@gmail.com>
2026-05-27 13:34:35 +0200

    Task 18 done

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,13 @@
+-- trigger to reset valid_email only when email changes
+DELIMITER $$
+
+CREATE TRIGGER reset_valid_email_before_update
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+
+DELIMITER ;
