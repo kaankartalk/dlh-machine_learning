f1f60c3b38f3faf8ee60abf70c948fddd0104a33
kaankartalk <kkaankartal@gmail.com>
2026-05-27 14:00:00 +0200

    Task 21 done

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,14 @@
+-- function to safely divide two integers
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END$$
+
+DELIMITER ;
