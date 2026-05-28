84fabbfe3c673047d7c0b9d5bac146aac67902ca
kaankartalk <kkaankartal@gmail.com>
2026-05-28 13:10:31 +0200

    Task 30 completed

---
+++ pipeline/databases/30-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for listing all documents in a MongoDB collection"""
+
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection
+    Args:
+        mongo_collection: pymongo collection object
+    Returns:
+        List of documents, or empty list if none
+    """
+    return list(mongo_collection.find())
