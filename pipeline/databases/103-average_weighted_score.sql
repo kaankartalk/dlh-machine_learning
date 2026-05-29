063633f048583fabadba744b364b075fccf99802
kaankartalk <kkaankartal@gmail.com>
2026-05-29 11:05:31 +0200

    Task 38 done

---
+++ pipeline/databases/103-average_weighted_score.sql
@@ -0,0 +1,16 @@
+-- Compute weighted average score for a user
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT SUM(c.score * p.weight) / SUM(p.weight)
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
+        WHERE c.user_id = user_id
+    )
+    WHERE id = user_id;
+END$$
+
+DELIMITER ;
