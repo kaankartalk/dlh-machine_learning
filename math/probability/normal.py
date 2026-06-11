c850d8623f7e7b944f456d025d0e88f4cec31232
kaankartalk <kkaankartal@gmail.com>
2026-06-11 15:22:25 +0000

    normal2

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -31,3 +31,11 @@ class Normal:
             # calculate standard deviation
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = variance ** 0.5
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        return (z * self.stddev) + self.mean
