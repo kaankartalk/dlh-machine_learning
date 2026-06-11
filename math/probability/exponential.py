5f7c288059616f7506efa3190fd99614436881ee
kaankartalk <kkaankartal@gmail.com>
2026-06-11 13:25:55 +0000

    poisson

---
+++ math/probability/exponential.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Exponential distribution class"""
+
+
+class Exponential:
+    """Represents an exponential distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Class constructor"""
+
+        if data is None:
+            # lambtha must be positive
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+
+        else:
+            # data must be a list
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+
+            # must contain at least 2 points
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            # exponential lambtha = 1 / mean
+            mean = sum(data) / len(data)
+            self.lambtha = 1 / mean
