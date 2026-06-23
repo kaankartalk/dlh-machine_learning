57568b60937040614b6d36217ac52cd0e73eab9f
kaankartalk <kkaankartal@gmail.com>
2026-06-23 12:54:01 +0000

    2-mar

---
+++ math/bayesian_prob/3-posterior.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def posterior(x, n, P, Pr):
+    """Calculates the posterior probability of each probability in P"""
+
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P"
+        )
+
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    fact = np.math.factorial
+    comb = fact(n) / (fact(x) * fact(n - x))
+
+    likelihood = comb * (P ** x) * ((1 - P) ** (n - x))
+    intersection = likelihood * Pr
+
+    marginal = np.sum(intersection)
+
+    return intersection / marginal
