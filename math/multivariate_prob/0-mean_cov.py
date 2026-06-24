ff054317aa261c4ebe67a3358a82c98d705a84a9
kaankartalk <kkaankartal@gmail.com>
2026-06-24 12:50:52 +0000

    Add content to README.md

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -2,7 +2,6 @@
 """
 Calculates a correlation matrix from a covariance matrix.
 """
-
 import numpy as np
 
 
@@ -10,11 +9,9 @@ def correlation(C):
     """Calculates a correlation matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-
     if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
-
     std = np.sqrt(np.diag(C))
     denom = np.outer(std, std)
     corr = C / denom
-    return corr
+    return corr```
