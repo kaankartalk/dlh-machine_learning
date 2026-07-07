bf7f34957cea733f508dff88a2af7118d2172446
Kaan Kartal Kuyucu <kaankartalkuyucu@mac.home>
2026-07-07 10:43:17 +0200

    0-from_numpy.py added

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 """Creates a pd.DataFrame from a np.ndarray"""
-import string
 import pandas as pd
 
 
 def from_numpy(array):
     """
     Creates a pd.DataFrame from a np.ndarray
-
-    array is the np.ndarray from which you should create the pd.DataFrame
-    The columns of the pd.DataFrame should be labeled in alphabetical
-    order and capitalized. There will not be more than 26 columns.
-
     Returns: the newly created pd.DataFrame
     """
-    columns = list(string.ascii_uppercase[:array.shape[1]])
+    columns = [chr(65 + i) for i in range(array.shape[1])]
     return pd.DataFrame(array, columns=columns)
